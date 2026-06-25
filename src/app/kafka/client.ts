26f0961950b090c139a3a8db350d5dbce8372c2f
Abir-Al-Arafat <antorabir@gmail.com>
2026-06-25 16:24:44 +0600

    kafka config through env

--- src/app/kafka/client.ts
+++ src/app/kafka/client.ts
@@ -3,5 +3,6 @@ import config from '../../config';
 
 export const kafka = new Kafka({
   clientId: 'hopper-roadside',
-  brokers: [`${config.ip}:9092`],
+  // brokers: [`${config.ip}:9092`],
+  brokers: [process.env.KAFKA_BROKERS || 'kafka:29092'],
 });
